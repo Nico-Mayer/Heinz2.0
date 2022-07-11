ac7af14f59594aff83e4d5d5dffafffaa46a5cf0
nico-mayer <nico.m94@icloud.com>
2022-07-11 10:14:35 +0200

    tile fix

--- components/Tile.jsx
+++ components/Tile.jsx
@@ -1,54 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import router from 'next/router'
 
-class Tile extends React.Component {
+function Tile({ title, text, imgSrc, url }) {
+  const [mouseOver, setMouseOver] = useState(false)
 
-    container = "";
-
-    title = "";
-    text="";
-    imgSrc="";
-    url="";
-
-    constructor(props) {
-        super(props);
-
-        this.title = this.props.title;
-        this.text = this.props.text;
-        this.imgSrc = this.props.imgSrc;
-        this.url = this.props.url;
-
-        this.container = <img className={"max-h-96 justify-self-center p-8 max-h-72"} src={this.imgSrc}/>;
-
-        this.handleMouseOut = this.handleMouseOut.bind(this);
-        this.handleMouseOver = this.handleMouseOver.bind(this);
-        this.state = {setIsHovering: false};
-    }
-
-    handleMouseOver = () => {
-        this.setState({setIsHovering: true});
-        this.container = <p className={"text-xl align-self-center justify-center"}>{this.text}</p>;
-    };
-
-    handleMouseOut = () => {
-        this.setState({setIsHovering: false});
-        this.container = <img className={"max-h-96 justify-self-center p-8 max-h-72"} src={this.imgSrc}/>;
-    };
-
-    render() {
-
-        return (
-            <>
-                <div className={"flex transition duration-400 hover:opacity-100 hover:opacity-100 hover:bg-heinzBlau w-1/2 justify-center pt-4"}
-                onClick={()=>{ router.push(this.url);}}>
-                    <div onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} className={"flex-column h-96"}>
-                        <h1 className={"justify-self-center text-3xl"}>{this.title}</h1>
-                        {this.container}
-                    </div>
-                </div>
-            </>
-        );
-    }
+  return (
+    <div
+      className={
+        'flex h-96 w-1/2 cursor-pointer flex-col items-center pt-4 transition-all duration-500 hover:bg-heinzBlau hover:opacity-100'
+      }
+      onClick={() => {
+        router.push(url)
+      }}
+      onMouseEnter={() => setMouseOver(true)}
+      onMouseLeave={() => setMouseOver(false)}
+    >
+      <h1 className={'text-3xl'}>{title}</h1>
+      <div className="my-auto">
+        {mouseOver ? (
+          <p className="mb-10 text-xl">{text}</p>
+        ) : (
+          <img className="max-h-72 justify-self-center" src={`${imgSrc}`} />
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Tile
