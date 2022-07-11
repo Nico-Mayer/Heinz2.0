21801bf4a4fa541a8a7cd1c551ae581b4b33e66e
nico-mayer <nico.m94@icloud.com>
2022-07-11 10:59:57 +0200

    Questtion Component improved

--- components/Question.jsx
+++ components/Question.jsx
@@ -1,74 +1,62 @@
-import React from 'react'
-import StyledText from "./StyledText";
-import {motion} from 'framer-motion'
+import React, { useState } from 'react'
+import StyledText from './StyledText'
+import { motion } from 'framer-motion'
 
-class Question extends React.Component {
-    text = "";
-    faq = "";
-    src = "/pfeilU.png";
+function Question({ text, faq }) {
+  const [showQuestion, setShowQuestion] = useState(false)
+  return (
+    <>
+      <div
+        className="flex cursor-pointer items-center "
+        onClick={() => setShowQuestion(!showQuestion)}
+      >
+        <h1 className="mb-0 w-full w-fit border-b-2 border-solid border-heinzBlau text-3xl text-heinzBlau transition duration-300 hover:border-white">
+          {faq}
+        </h1>
+        <img
+          src="/pfeilU.png"
+          alt="arrow"
+          className={
+            `ml-8 h-6 transition-all duration-300 ` +
+            (showQuestion ? 'rotate-180' : '')
+          }
+        />
+      </div>
 
-    constructor(props) {
-        super(props);
-        this.text = this.props.text;
-        this.faq = this.props.faq;
-        this.state = {showQuestion: false};
-        this.toggleQuestion = this.toggleQuestion.bind(this);
-    }
-
-    toggleQuestion() {
-        if (this.state.showQuestion) {
-            this.setState({showQuestion: false})
-            this.src = "/pfeilU.png";
-        } else {
-            this.setState({showQuestion: true})
-            this.src = "/pfeilO.png";
-        }
-    }
-
-    render() {
-        let showQuestion = this.state.showQuestion;
-        let container;
-        if (showQuestion) {
-            container = <motion.div
-                initial="hidden"
-                animate="visible"
-                variants={{
-                    hidden: {
-                        scale: 0.8,
-                        opacity: 0,
-                        height: 0,
-                    },
-                    visible: {
-                        scale: 1,
-                        opacity: 1,
-                        height: "auto",
-                        transition: {
-                            delay: 0.05,
-                        },
-                    },
-                }} className={"bg-heinzBlau max-w-3xl mt-0s"}><StyledText
-                text={<p className={'whitespace-pre-wrap text-left text-white'}>
-                    {this.text}
-                </p>}/></motion.div>
-        } else {
-            container = <></>;
-        }
-
-        return (
-            <>
-                <div
-                    className={
-                        'flex flex-row flex-nowrap content-evenly justify-center '
-                    }
-                >
-                    <h1 onClick={this.toggleQuestion}
-                        className={"text-heinzBlau text-3xl w-full max-w-3xl mb-0 border-solid border-b-2 border-heinzBlau"}>{this.faq}</h1>
-                    <img onClick={this.toggleQuestion} className={'max-h-10'} src={this.src}/>
-                </div>
-                {container}
-            </>
-        );
-    }
+      {showQuestion ? (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+              height: 0,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              height: 'auto',
+              transition: {
+                delay: 0.05,
+              },
+            },
+          }}
+          className={`mt-0s max-w-3xl bg-heinzBlau ${
+            showQuestion ? 'block' : 'hidden'
+          }`}
+        >
+          <StyledText
+            text={
+              <p className={'whitespace-pre-wrap text-left text-white'}>
+                {text}
+              </p>
+            }
+          />
+        </motion.div>
+      ) : null}
+    </>
+  )
 }
 
 export default Question
