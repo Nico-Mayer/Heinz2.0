204dead458686d115f66cd9b1d795c2498df90b3
nico-mayer <nico.m94@icloud.com>
2022-07-17 10:35:05 +0200

    Download button added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { motion } from 'framer-motion'
 import Tile from '../components/Tile'
-import router from "next/router";
+import router from 'next/router'
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <title>Heinz 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="mx-auto flex h-full w-full max-w-7xl flex-col  text-center text-white">
+      <main className="mx-auto mb-24 flex h-full w-full max-w-7xl  flex-col text-center text-white">
         <div className="">
           <motion.div
             initial="hidden"
@@ -35,6 +35,24 @@ const Home: NextPage = () => {
               Tauche ein in die Welt der Deepfakes!
             </h1>
           </motion.div>
+          <div className="my-10 flex w-full flex-col items-center">
+            <a
+              href="https://drive.google.com/file/d/1wwAFxBSY4hMnQc1c02sKuKRf70d7Hrb4/view?usp=sharing"
+              target="blank"
+              className="mb-2 flex rounded bg-heinzBlau/90 px-6 py-4 hover:bg-heinzBlau"
+            >
+              <img
+                src="https://api.iconify.design/material-symbols:cloud-download.svg?color=%23ffffff"
+                alt="download arrow"
+                className="mr-2 h-6 w-6 text-white"
+              />
+              Download
+            </a>
+            <p className="m-0 font-mono text-sm opacity-50">
+              Heinz2.0 Deepface Live Model
+            </p>
+          </div>
+
           <div
             className={
               'z-10 mt-12 flex h-min flex-wrap justify-center bg-bgMain/40 shadow-3xl backdrop-blur-md '
@@ -75,9 +93,12 @@ const Home: NextPage = () => {
                 'duration-400 flex w-1/2 cursor-pointer justify-center pt-4 opacity-50 transition hover:bg-heinzBlau hover:opacity-100 hover:opacity-100'
               }
             >
-              <div className={'flex-column'} onClick={() => {
-                router.push('/sub/faq')
-              }}>
+              <div
+                className={'flex-column'}
+                onClick={() => {
+                  router.push('/sub/faq')
+                }}
+              >
                 <h1 className={'justify-self-center text-3xl'}>FAQ</h1>
                 <p className={'max-h-96 justify-self-center p-8 text-9xl'}>?</p>
               </div>
